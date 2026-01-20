f8d12330724a965a4dffb712d5b1b9faa5494d81
khairrahmanto <khair_rahmanto_34rpl@student.smktelkom-mlg.sch.id>
2026-01-20 15:13:45 +0700

    Add files via upload

---
+++ favorite.php
@@ -0,0 +1,22 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <head>
+     <body>
+        <h1>Lagune wager</h1>
+        <ol type="1">
+ <li><iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/6ABeRdBKgEhOmuQZ7OOGT2?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+ <li><iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/4kHJ9VXOoprNxzGBMzaTw6?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+ <li><iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/2gANywSFYF58YFMPdDSAjC?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+ <li><iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/track/5JESuE80fvausWHjcT9QVh?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe></li>
+ <li><iframe data-testid="embed-iframe" style="border-radius:12px" src=https://open.spotify.com/embed/track/4PMqSO5qyjpvzhlLI5GnID?utm_source=generator width=100% height=352 frameBorder=0 allowfullscreen="" allow=autoplay\, clipboard-write\, encrypted-media\, fullscreen\, picture-in-picture loading=lazy></iframe></li>
+    <h1>wager punya yt</h1>
+    <ol type ="1">
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/lIzju0oigRY?si=6MFJFE39Kx5q3inf" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></li>
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/5BLW1dFIS0U?si=4Zwyt5pVOdxpuuIZ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></li>
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/XKnpSuRKYQ4?si=cF2USvAm6lcnTj4U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></li>
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/u2tiiyYtA8w?si=Ewi37QhkyxX2THjn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></li>
+<li><iframe width="560" height="315" src="https://www.youtube.com/embed/2cikuVFc3mw?si=EhnnQ0RdeY31hO8B" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></li>
